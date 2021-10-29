e69f2a615bd1d8d9f57281915a00b295106ba4d8
SheidakMaryia <mashasheidak@mail.ru>
2021-10-29 22:14:40 +0300

    first commit

---
+++ src/main/java/com/example/food_delivery_diploma/FoodDeliveryDiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_delivery_diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryDiplomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryDiplomaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/food_delivery_diploma/FoodDeliveryDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_delivery_diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
